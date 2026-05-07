809cff6109696e41adab3c433aac9c4923658e37
RaySiGZ <96760522+RaySiGZ@users.noreply.github.com>
2026-05-07 20:49:53 +0500

    Update UnitTest.cs
    Formating CorrectTestData expected string

--- ArrayCompressor.Test/UnitTest.cs
+++ ArrayCompressor.Test/UnitTest.cs
@@ -8,10 +8,10 @@ namespace ArrayCompressor.Test
    {
       public static IEnumerable<string[]> CorrectTestData =>
          [
-            [ "[1, 1, 2, 2, 3]", "1, 2, 3" ],
-            [ "[0, 0, 1, 1, 0]", "0, 1, 0" ],
-            [ "[-1, -1, -1, 2, -1, -1, 3]", "-1, 2, -1, 3" ],
-            [ "[]", "" ]
+            [ "[1, 1, 2, 2, 3]", "[1, 2, 3]" ],
+            [ "[0, 0, 1, 1, 0]", "[0, 1, 0]" ],
+            [ "[-1, -1, -1, 2, -1, -1, 3]", "[-1, 2, -1, 3]" ],
+            [ "[]", "[]" ]
          ];
 
       public static IEnumerable<object[]> InCorrectTestData =>
