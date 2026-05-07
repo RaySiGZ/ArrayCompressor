ad2884bdfb8da3304202e1fcf710a5a85d3dff3b
RaySiGZ <levfed2001@gmail.com>
2026-05-07 20:23:57 +0500

    Public ArrayCompressor

---
+++ ArrayCompressor.Test/UnitTest.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+
+using static ArrayCompressor.Program;
+
+namespace ArrayCompressor.Test
+{
+   public class Tests
+   {
+      public static IEnumerable<string[]> CorrectTestData =>
+         [
+            [ "[1, 1, 2, 2, 3]", "1, 2, 3" ],
+            [ "[0, 0, 1, 1, 0]", "0, 1, 0" ],
+            [ "[-1, -1, -1, 2, -1, -1, 3]", "-1, 2, -1, 3" ],
+            [ "[]", "" ]
+         ];
+
+      public static IEnumerable<object[]> InCorrectTestData =>
+         [
+            [ "]" ],
+            [ "a" ],
+            [ "[a, -1, -1, 2, -1, -1, 3]" ],
+            [ "[1 2 3]" ],
+            [ "]" ],
+            [ "" ],
+         ];
+
+      [Theory]
+      [MemberData(nameof(CorrectTestData))]
+      public void CorrectProcessing(string input, string expected) => Assert.Equal(expected, ProcessingInput(input));
+
+      [Theory]
+      [MemberData(nameof(InCorrectTestData))]
+      public void InCorrectProcessing(string input) => Assert.Equal("Incorrect input", ProcessingInput(input));
+
+      [Fact]
+      public void EmptyArrayCompressor() => Assert.Equal([], CompressArray([]));
+
+      [Fact]
+      public void NullArrayCompressor() => Assert.Equal([], CompressArray(null!));
+   }
+}
---
+++ ArrayCompressor/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ArrayCompressor;
+
+public class Program
+{
+   public  static int[] CompressArray(int[] input)
+   {
+      if (input == null || input.Length == 0) return [];
+
+      List<int> inputDistinct = [];
+
+      inputDistinct.Add(input[0]);
+
+      for (int i = 1; i < input.Length; i++)
+         if (input[i] != inputDistinct[^1]) inputDistinct.Add(input[i]);
+
+      return [.. inputDistinct];
+   }
+
+   public static string ProcessingInput(string input)
+   {
+      if (string.IsNullOrEmpty(input) || !input.StartsWith('[') || !input.EndsWith(']')) return "Incorrect input";
+
+      if (input == "[]") return string.Empty;
+
+      int?[] inputSplit = input.Trim('[', ']', ' ').Split(',').Select(x => int.TryParse(x, out int z) ? z : (int?)null).ToArray();
+
+      if (inputSplit.Any(x => x == (int?)null)) return "Incorrect input";
+
+#pragma warning disable CS8629 // Проверка выполняется шагом ранее.
+      return string.Join(", ", CompressArray([.. inputSplit.Select(x => (int)x.Value)]));
+#pragma warning restore CS8629 // Тип значения, допускающего NULL, может быть NULL.
+   }
+
+   static void Main()
+   {
+      Console.WriteLine(@"Send array -> [-1, 0, 0, 1, 1, -1]
+or
+Send `exit` to close program");
+
+      string _input = Console.ReadLine() ?? string.Empty;
+
+      while (!_input.Equals("exit", StringComparison.CurrentCultureIgnoreCase))
+      {
+         Console.WriteLine(ProcessingInput(_input));
+         _input = Console.ReadLine() ?? string.Empty;
+      }
+   }
+}
